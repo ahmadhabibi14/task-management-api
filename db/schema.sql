e91bdae3eca9245ccc56eaab5d850ca138264f7f
Ahmad Habibi <ahmadhabibi7159@gmail.com>
2024-11-08 16:47:58 +0800

    [chore] bump db

--- db/schema.sql
+++ db/schema.sql
@@ -20,7 +20,7 @@ CREATE TABLE `schema_migrations` (
 
 LOCK TABLES `schema_migrations` WRITE;
 /*!40000 ALTER TABLE `schema_migrations` DISABLE KEYS */;
-INSERT INTO `schema_migrations` VALUES ('20241107113120');
+INSERT INTO `schema_migrations` VALUES ('20241107113120'),('20241107155620');
 /*!40000 ALTER TABLE `schema_migrations` ENABLE KEYS */;
 UNLOCK TABLES;
 DROP TABLE IF EXISTS `tasks`;
@@ -34,13 +34,14 @@ CREATE TABLE `tasks` (
   `status` varchar(255) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `is_deleted` tinyint(1) NOT NULL DEFAULT 0,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 LOCK TABLES `tasks` WRITE;
 /*!40000 ALTER TABLE `tasks` DISABLE KEYS */;
-INSERT INTO `tasks` VALUES (1,'Apply Job','Apply a Backend Developer job to ASAP Lombok','2024-11-07','in-progress','2024-11-07 12:28:10','2024-11-07 12:28:10'),(2,'Apply Job','Apply a Backend Developer job to ASAP Lombok','2024-11-07','in-progress','2024-11-07 12:28:39','2024-11-07 12:28:39'),(3,'Apply Job','Apply a Backend Developer job to ASAP Lombok','2024-11-07','in-progress','2024-11-07 12:29:19','2024-11-07 12:29:19'),(4,'Apply Job','Apply a Backend Developer job to ASAP Lombok','2024-11-07','in-progress','2024-11-07 12:40:00','2024-11-07 12:40:00'),(5,'Apply Job','Apply a Backend Developer job to ASAP Lombok','2024-11-07','in-progress','2024-11-07 12:41:30','2024-11-07 12:41:30'),(6,'Apply Job','Apply a Backend Developer job to ASAP Lombok','2024-11-07','in-progress','2024-11-07 13:10:16','2024-11-07 13:10:16');
+INSERT INTO `tasks` VALUES (1,'makan','Apply a Backend Developer job to ASAP Lombok','2024-11-07','in-progress','2024-11-07 12:28:10','2024-11-08 08:06:02',1),(2,'Apply Job','Apply a Backend Developer job to ASAP Lombok','2024-11-07','in-progress','2024-11-07 12:28:39','2024-11-07 12:28:39',0),(3,'Makan','Aku makan Bakso','2024-11-09','in-progress','2024-11-07 12:29:19','2024-11-07 16:08:53',0),(4,'Apply Job','Apply a Backend Developer job to ASAP Lombok','2024-11-07','pending','2024-11-07 12:40:00','2024-11-08 08:37:47',1),(5,'Apply Job','Apply a Backend Developer job to ASAP Lombok','2024-11-07','in-progress','2024-11-07 12:41:30','2024-11-07 12:41:30',0),(6,'Apply Job','Apply a Backend Developer job to ASAP Lombok','2024-11-07','in-progress','2024-11-07 13:10:16','2024-11-07 13:10:16',0);
 /*!40000 ALTER TABLE `tasks` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
